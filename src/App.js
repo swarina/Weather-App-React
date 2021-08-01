6ccc639e3a15fc416ef511c9e9e4b60dca3038f2
swarina <swarinajaiswal@gmail.com>
2021-08-01 21:06:06 +0530

    Add: Date, day, Location display

--- src/App.js
+++ src/App.js
@@ -6,6 +6,39 @@ const api = {
   base: "https://api.openweathermap.org/data/2.5/",
 };
 
+const dateBuilder = (d) => {
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day} ${date} ${month} ${year}`;
+};
+
 function App() {
   return (
     <div className="app">
@@ -16,6 +49,10 @@ function App() {
             className="search-bar"
             placeholder="Enter Location"
           />
+        </div>
+        <div className="location-box">
+          <div className="location">India</div>
+          <div className="date">{dateBuilder(new Date())}</div>
         </div>
       </main>
     </div>
