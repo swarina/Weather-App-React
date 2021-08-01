142e88803b645587a7ad06d6178a7d70f9a588b1
swarina <swarinajaiswal@gmail.com>
2021-08-01 20:28:40 +0530

    Add necessary styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React from "react";
+
+//API
+const api = {
+  key: "e32b604e9e53656e3f87f1ff8f73ccfe",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Enter Location"
+          />
+        </div>
+      </main>
     </div>
   );
 }
